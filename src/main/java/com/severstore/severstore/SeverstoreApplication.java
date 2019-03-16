4601fc8bca2df5f507b3ff3648c41e6a51764e04
Pavel <Pavel.WebOnline@gmail.com>
2019-03-17 01:06:52 +0300

    Start

---
+++ src/main/java/com/severstore/severstore/SeverstoreApplication.java
@@ -0,0 +1,13 @@
+package com.severstore.severstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverstoreApplication.class, args);
+	}
+
+}
