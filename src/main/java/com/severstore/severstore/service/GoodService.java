de90ff7300f82ebe490199c08361be59e279dade
Pavel <Pavel.WebOnline@gmail.com>
2019-03-17 20:46:59 +0300

    Added functional to good service
    It need for invokes CRUD operations

--- src/main/java/com/severstore/severstore/service/GoodService.java
+++ src/main/java/com/severstore/severstore/service/GoodService.java
@@ -1,4 +1,17 @@
 package com.severstore.severstore.service;
 
+import com.severstore.severstore.entity.GoodEntity;
+
+import java.util.List;
+
 public interface GoodService {
+
+    GoodEntity getById(Long idGood);
+
+    List<GoodEntity> getAll();
+
+    GoodEntity save(GoodEntity goodEntity);
+
+    void delete(GoodEntity goodEntity);
+
 }
--- src/main/java/com/severstore/severstore/service/impl/GoodServiceImpl.java
+++ src/main/java/com/severstore/severstore/service/impl/GoodServiceImpl.java
@@ -1,9 +1,36 @@
 package com.severstore.severstore.service.impl;
 
+import com.severstore.severstore.dao.GoodRepository;
+import com.severstore.severstore.entity.GoodEntity;
 import com.severstore.severstore.service.GoodService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GoodServiceImpl implements GoodService {
 
+    @Autowired
+    GoodRepository goodRepository;
+
+    @Override
+    public GoodEntity getById(Long idGood) {
+        return goodRepository.findById(idGood).get();
+    }
+
+    @Override
+    public List<GoodEntity> getAll() {
+        return goodRepository.findAll();
+    }
+
+    @Override
+    public GoodEntity save(GoodEntity goodEntity) {
+        return goodRepository.save(goodEntity);
+    }
+
+    @Override
+    public void delete(GoodEntity goodEntity) {
+        goodRepository.delete(goodEntity);
+    }
 }
