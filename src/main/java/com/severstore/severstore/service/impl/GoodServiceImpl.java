eb9d8b89ef8caae96e70b612ec6b327e22abb8d7
Pavel <Pavel.WebOnline@gmail.com>
2019-03-17 20:06:11 +0300

    Added service annotation

--- src/main/java/com/severstore/severstore/service/impl/GoodServiceImpl.java
+++ src/main/java/com/severstore/severstore/service/impl/GoodServiceImpl.java
@@ -1,7 +1,9 @@
 package com.severstore.severstore.service.impl;
 
 import com.severstore.severstore.service.GoodService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GoodServiceImpl implements GoodService {
 
 }
--- src/main/java/com/severstore/severstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/severstore/severstore/service/impl/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.severstore.severstore.service.impl;
 
 import com.severstore.severstore.service.OrderService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderServiceImpl implements OrderService {
 
 }
