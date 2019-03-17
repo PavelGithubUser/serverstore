5bdaee2e75320bf00f925a5cbd30919657083551
Pavel <Pavel.WebOnline@gmail.com>
2019-03-18 02:26:02 +0300

    Added functional of goods controller
    It need for pass good DTO to client and to back

--- src/main/java/com/severstore/severstore/controller/GoodController.java
+++ src/main/java/com/severstore/severstore/controller/GoodController.java
@@ -1,10 +1,46 @@
 package com.severstore.severstore.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.severstore.severstore.dto.GoodDTO;
+import com.severstore.severstore.entity.GoodEntity;
+import com.severstore.severstore.service.GoodService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/good")
 public class GoodController {
+
+    @Autowired
+    GoodService goodService;
+
+    @GetMapping(value = "/get/{id}")
+    public GoodDTO getGoodById(@PathVariable("id") Long id){
+        return new ModelMapper().map(goodService.getById(id), GoodDTO.class);
+    }
+
+    @PostMapping(value = "/save")
+    public GoodDTO getSave(@RequestBody GoodDTO goodDTO){
+        ModelMapper modelMapper = new ModelMapper();
+        GoodEntity goodEntity = modelMapper.map(goodDTO, GoodEntity.class);
+        return modelMapper.map(goodService.save(goodEntity), GoodDTO.class);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public boolean getDeleteById(@PathVariable("id") Long id){
+        return goodService.deleteById(id);
+    }
+
+    @GetMapping(value = "/all")
+    public List<GoodDTO> getGoodsList(){
+        ModelMapper modelMapper = new ModelMapper();
+        return goodService.getAll()
+                .stream()
+                .map(goodEntity -> modelMapper.map(goodEntity, GoodDTO.class))
+                .collect(Collectors.toList());
+    }
 
 }
