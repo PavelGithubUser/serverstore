667402ed58f70153844c597e9d90f83895d840d6
Pavel <Pavel.WebOnline@gmail.com>
2019-03-17 20:53:19 +0300

    Added functional to order service
    It need for execute CRUD operations for orders

--- src/main/java/com/severstore/severstore/service/OrderService.java
+++ src/main/java/com/severstore/severstore/service/OrderService.java
@@ -1,4 +1,17 @@
 package com.severstore.severstore.service;
 
+import com.severstore.severstore.entity.OrderEntity;
+
+import java.util.List;
+
 public interface OrderService {
+
+    OrderEntity getById(Long idOrder);
+
+    List<OrderEntity> getAll();
+
+    OrderEntity save(OrderEntity orderEntity);
+
+    void delete(OrderEntity orderEntity);
+
 }
--- src/main/java/com/severstore/severstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/severstore/severstore/service/impl/OrderServiceImpl.java
@@ -1,9 +1,36 @@
 package com.severstore.severstore.service.impl;
 
+import com.severstore.severstore.dao.OrderRepository;
+import com.severstore.severstore.entity.OrderEntity;
 import com.severstore.severstore.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Override
+    public OrderEntity getById(Long idOrder) {
+        return orderRepository.findById(idOrder).get();
+    }
+
+    @Override
+    public List<OrderEntity> getAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public OrderEntity save(OrderEntity orderEntity) {
+        return orderRepository.save(orderEntity);
+    }
+
+    @Override
+    public void delete(OrderEntity orderEntity) {
+        orderRepository.delete(orderEntity);
+    }
 }
